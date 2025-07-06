2a31478a16da866a5794fcc61b3a6d4a873d3196
MIKI <mikimiki2002.ma@gmail.com>
2025-07-06 18:19:50 +0900

    routesにauthとuserの処理の追加

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,14 @@
 
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const multer = require("multer");
+
+const bcrypt = require("bcrypt"); //パスワードのハッシュ化
 
 const router = express.Router();
 
@@ -15,20 +18,11 @@ const getDBConnection = require("../db");
 const USER_PARAMETER_ERROR = 400
 const SERVER_ERROR = 500
 
-router.post("/login", (req, res) => {
-  // ログイン処理
-  const { username, password } = req.body;
+// 第三者の改竄を防ぐためのサーバー側の秘密鍵
+const SECRET_KEY = "quest_calendar_secret_key#123";
 
-  if (username === "user" && password === "pass") {
-    req.session.user = { username };
-    res.json({ message: "ログイン成功" });
-  } else {
-    res.status(401).json({ message: "ログイン失敗" });
-  }
-});
-
-router.post("/register", async (req, res) => {
-  // 登録処理
+// ログイン処理
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
@@ -37,66 +31,84 @@ router.post("/register", async (req, res) => {
   }
 
   try {
+     const db = await getDBConnection();
+
+    // ユーザー名で検索
+    const user = await db.get("SELECT * FROM user WHERE username = ?", username);
+    console.log(user)
+
+    if (!user) {
+      return res.status(USER_PARAMETER_ERROR).send("ユーザーが存在しません");
+    }
+
+    // パスワードの比較
+    const is_valid_password = await bcrypt.compare(password, user.password);
+    if (!is_valid_password) {
+      return res.status(USER_PARAMETER_ERROR).send("パスワードが違います");
+    }
+
+    // 認証tokenを返す
+    const token = jwt.sign({ userId: user.user_id, username: user.username }, SECRET_KEY, { expiresIn: "1h" });
+    res.json({ "token" : token, "user": username});
+
+  } catch (err) {
+    console.log(err)
+    res.status(SERVER_ERROR).type("text")
+      .send("サーバーに問題が起きました。もう一度試してください。");
+  }
+
+});
+
+// 登録処理
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(USER_PARAMETER_ERROR).type('text')
+      .send("ユーザーネームまたはパスワードが空です")
+  }
+
+  try {
     const db = await getDBConnection();
+
+    // すでにこのusernameがデータベースにあるか確認
     const duplicateUserName = await findDuplicateUserName(db, username);
     if (duplicateUserName) {
       await db.close();
       return res.status(USER_PARAMETER_ERROR).type("text")
-        .send(`This username has been registered`);
+        .send(`このユーザーネームはすでに使われています`);
     } else {
 
+      //パスワードをハッシュ化
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // データベースに挿入
       const query = `INSERT INTO user (username, password) VALUES (?, ?)`
+      await db.run(query, [username, hashedPassword]);
 
-      await db.run(query, [username, password]);
-
+      // 成功メッセージを返す
       await db.close();
-      res.type("text").send("successful");
+      res.type("text").send("登録成功しました！下からログインしてください");
     }
 
   } catch (err) {
     console.log(err)
     res.status(SERVER_ERROR).type("text")
-      .send("Something is wrong with the server");
+      .send("サーバーに問題がありました。もう一度試してください。");
   }
 });
 
+// ログアウト処理
 router.post("/logout", (req, res) => {
-  req.session.destroy(() => {
+  req.session.destroy(err => {
+    if (err) return res.status(500).send("ログアウト失敗");
     res.clearCookie("connect.sid");
     res.json({ message: "ログアウト成功" });
   });
 });
 
-/**
- * Return all user's infomation
- */
-router.get("/all", async (req, res) => {
-  try {
-
-    let db = await getDBConnection();
-
-    // select all users from user table
-    let results = await db.all("SELECT * FROM user");
-    if (results.length > 0) {
-      let returnResults = {
-        "user": results
-      };
-      await db.close();
-      res.json(returnResults);
-
-    // no users in database
-    } else {
-      await db.close();
-      res.status(SERVER_ERROR).type('text')
-        .send('No users are in database. Please try again');
-    }
-  } catch (err) {
-    console.log(err)
-    res.status(SERVER_ERROR).type("text")
-      .send("Something is wrong with server. Please try again");
-  }
-});
-
+// すでにそのusernamaeが存在しているかを確認する関数
+// もしすでにあればtrue, なければfalseを返す
 async function findDuplicateUserName(db, username) {
   try {
     let usernameResult = await db.all("SELECT * FROM user WHERE username = ?", username);
