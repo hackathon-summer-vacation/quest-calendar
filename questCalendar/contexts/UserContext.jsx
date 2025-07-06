621059c1603767f9aa9566450c2c03cdb0e16bff
MIKI <mikimiki2002.ma@gmail.com>
2025-07-06 11:18:06 +0900

    login example code using fetch

--- questCalendar/contexts/UserContext.jsx
+++ questCalendar/contexts/UserContext.jsx
@@ -6,7 +6,21 @@ export function UserProvider({ children }) {
   const [user, setUser] = useState(null)
 
   async function login(username, password) {
-    
+    try {
+      const res = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      if (!res.ok) throw new Error("Login failed")
+      const data = await res.json()
+      setUser(data.user)
+    } catch (err) {
+      console.log("error when logging in")
+    }
   }
 
   async function register(username, password) {
