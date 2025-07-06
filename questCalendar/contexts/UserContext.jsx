436e659276c6bbda3ac43a4593a2020575a40a48
MIKI <mikimiki2002.ma@gmail.com>
2025-07-06 18:19:12 +0900

    コメント追加

--- questCalendar/contexts/UserContext.jsx
+++ questCalendar/contexts/UserContext.jsx
@@ -5,9 +5,11 @@ export const UserContext = createContext()
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null)
 
+  // ログイン処理 resには成功したら{"token": token, "user": user}のjsonファイルが返ってくる
+  // 失敗すると、失敗メッセージが返ってくる
   async function login(username, password) {
     try {
-      const res = await fetch("http://localhost:8000/login", {
+      const res = await fetch("http://localhost:8000/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -15,18 +17,24 @@ export function UserProvider({ children }) {
         body: JSON.stringify({ username, password })
       });
 
-      if (!res.ok) throw new Error("Login failed")
-      const data = await res.json()
-      console.log(data)
-      setUser(data.user)
+      if (!res.ok) {
+        message = await res.text()
+        return message
+      } else {
+        const data = await res.json()
+        setUser(data.user)
+        return data
+      }
     } catch (err) {
+      console.log(err)
       console.log("error when logging in")
     }
   }
 
+  // 登録処理、 登録できれば、成功メッセージ、できなければ失敗メッセージを返す
   async function register(username, password) {
     try {
-      const res = await fetch("http://localhost:8000/register", {
+      const res = await fetch("http://localhost:8000/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -34,16 +42,17 @@ export function UserProvider({ children }) {
         body: JSON.stringify({ username, password })
       });
 
-      if (!res.ok) throw new Error("Register failed")
-      const data = await res.json()
+      message = await res.text()
+      return message
     } catch (err) {
-      console.log("error when registering")
+      return "サーバーで問題が起きました。もう一度試してください。"
     }
   }
 
+  // ログアウト処理 (未完成)
   async function logout() {
     try {
-      const res = await fetch("http://localhost:8000/logout", {
+      const res = await fetch("http://localhost:8000/auth/logout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
